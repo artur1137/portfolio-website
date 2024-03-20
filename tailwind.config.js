27e2621489a5a6bd01ff80fc7700434952bde0ff
Artur <osiakowski.a@gmail.com>
2024-03-20 12:40:04 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: [
+    "./templates/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        supermolot: ['"TT Supermolot Neue Exp DBold"']
+      },
+    },
+  },
+  plugins: [],
+  darkMode: ['selector', '[data-mode="dark"]'],
+}
